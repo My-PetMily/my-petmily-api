44d5d2dd11c4bc5fea73a3f17a78bc87f12eb5a0
SuJeongGong <0_sujeong@naver.com>
2023-02-19 03:05:42 +0900

    env: DB 연결 설정 #2
    1. DB 연결 설정을 위한 의존성 추가
    2. DB 로깅 설정 완료
    3. DB 연결과 설정을 위한 테스트 코드 작성ㅕ

---
+++ src/main/java/com/family/mypetmily/MyPetMilyApplication.java
@@ -0,0 +1,13 @@
+package com.family.mypetmily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetMilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetMilyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/family/mypetmily/MyPetMilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.family.mypetmily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetMilyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
