9f02a51cf709028450e4b06312b5661136a2ccdc
SuJeongGong <0_sujeong@naver.com>
2023-02-24 20:09:58 +0900

    env: swagger2 설정 #4
    swagger3 -> swagger2 변경

--- src/main/java/com/family/mypetmily/response/model/ApiResponseDto.java
+++ src/main/java/com/family/mypetmily/response/model/ApiResponseDto.java
@@ -1,6 +1,7 @@
 package com.family.mypetmily.response.model;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,31 +20,31 @@ import java.util.Map;
  */
 @Getter
 @Setter
-@Schema(name = "공통 Response", description = "모든 API의 공통적인 응답 명세")
-public class ApiResponse {
+@ApiModel(value = "공통 Response", description = "모든 API의 공통적인 응답 명세")
+public class ApiResponseDto {
 
 	/**
 	 * 결과값에 대한 코드
 	 * 코드가 021이라면 앞의 0도같이 전달하기 위해 String 타입으로 정의
 	 */
-	@Schema(description = "결과값에 대한 코드\n코드가 021이라면 앞의 0도같이 전달하기 위해 String 타입으로 정의", example = "-401")
+	@ApiModelProperty(value = "결과값에 대한 코드\n코드가 021이라면 앞의 0도같이 전달하기 위해 String 타입으로 정의", example = "-401")
 	private String code;
 
 	/**
 	 * 결과값에 대한 설명을 나타내는 메세지
 	 * 에러시에는 에러 원인에 대한 참고 정보로써 변경될 수 있으므로, 예외 처리시에는 예외 코드를 이용할 것
 	 */
-	@Schema(description = "결과값에 대한 설명을 나타내는 메세지\n에러시에는 에러 원인에 대한 참고 정보로써 변경될 수 있으므로, 예외 처리시에는 예외 코드를 이용할 것", example = "토큰을 잘못 보냈습니다.")
+	@ApiModelProperty(value = "결과값에 대한 설명을 나타내는 메세지\n에러시에는 에러 원인에 대한 참고 정보로써 변경될 수 있으므로, 예외 처리시에는 예외 코드를 이용할 것", example = "토큰을 잘못 보냈습니다.")
 	private String message;
 
 	/**
 	 * 요청에 대한 정보
 	 * 어떤 요청 결과 값이든 참조 할 수 있도록 Object 타입으로 정의
 	 */
-	@Schema(description = "요청에 대한 정보\n어떤 요청 결과 값이든 참조 할 수 있도록 Object 타입으로 정의", example = "{\n\t\t\"token\": \"잘못 보낸 토큰 정보\"\n\t}")
+	@ApiModelProperty(value = "요청에 대한 정보\n어떤 요청 결과 값이든 참조 할 수 있도록 Object 타입으로 정의", example = "{\n\t\t\"token\": \"잘못 보낸 토큰 정보\"\n\t}")
 	private Object result;
 
-	public ApiResponse(String code, String message, Object result) {
+	public ApiResponseDto(String code, String message, Object result) {
 		if (code == null || code.isEmpty()) {
 			this.code = "";
 		} else {
@@ -63,7 +64,7 @@ public class ApiResponse {
 		}
 	}
 
-	public ApiResponse(String code, String message) {
+	public ApiResponseDto(String code, String message) {
 		this.code = code;
 		this.message = message;
 
@@ -77,7 +78,7 @@ public class ApiResponse {
 		}
 	}
 
-	public ApiResponse() {
+	public ApiResponseDto() {
 		this(null, null, null);
 	}
 
--- src/main/java/com/family/mypetmily/user/controller/AuthController.java
+++ src/main/java/com/family/mypetmily/user/controller/AuthController.java
@@ -1,17 +1,23 @@
 package com.family.mypetmily.user.controller;
 
-import com.family.mypetmily.response.model.ApiResponse;
+import com.family.mypetmily.response.model.ApiResponseDto;
 import com.family.mypetmily.user.service.inter.AuthService;
 import com.family.mypetmily.validation.exception.EmptyValueException;
 import com.family.mypetmily.validation.exception.InvalidValueException;
 import com.family.mypetmily.validation.exception.ValueException;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Pattern;
 
 /**
  * description      : 유저 관련 인증 컨트롤러
@@ -24,7 +30,8 @@ import org.springframework.web.bind.annotation.RestController;
  * 2023-02-19       SuJeong Gong        최초작성
  */
 
-@Tag(name = "User Auth API", description = "유저 관련 인증, 중복확인 API")
+
+@Api(tags = "User Auth API", description = "유저 관련 인증, 중복확인 API")
 @RestController("/auth")
 @AllArgsConstructor
 @Slf4j
@@ -37,27 +44,31 @@ public class AuthController {
 	 * @param nickname 테스트 할 닉네임
 	 * @return 결과
 	 */
+	@ApiOperation(value = "닉네임 중복 조회 API", notes = "닉네임을 변경하기 전에 사용하는 사람이 있는지, 혹은 닉네임이 조건에 맞는지 확인 해 볼 수 있는 API")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "조회 성공")
+	})
 	@GetMapping("/nickname")
-	public ResponseEntity<ApiResponse> checkNickname(@RequestParam("nickname") final String nickname) {
-		ApiResponse response = null;
+	public ResponseEntity<ApiResponseDto> checkNickname(@RequestParam("nickname") @Pattern(regexp = "[\\d\\w\\_\\-\\.]{2,20}") final String nickname, BindingResult bindingResult) {
+		ApiResponseDto response = null;
 
 		try {
 			if (authService.canUseNickname(nickname)) {
-				response = new ApiResponse("200", "사용해도 좋습니다.");
+				response = new ApiResponseDto("200", "사용해도 좋습니다.");
 				log.info("사용해도 좋은 nickname 입니다. nickname = [{}]", nickname);
 			} else {
-				response = new ApiResponse("200", "이미 사용중인 닉네임 입니다.");
+				response = new ApiResponseDto("200", "이미 사용중인 닉네임 입니다.");
 				log.info("중복되는 nickname 입니다. nickname = [{}]", nickname);
 			}
 		} catch (ValueException e) {
 			if (e.getCause() instanceof EmptyValueException) {
 				EmptyValueException emptyValueException = (EmptyValueException) e.getCause();
-				response = new ApiResponse(emptyValueException.getCode(), emptyValueException.getMessage());
+				response = new ApiResponseDto(emptyValueException.getCode(), emptyValueException.getMessage());
 			} else if (e.getCause() instanceof InvalidValueException) {
 				InvalidValueException invalidValueException = (InvalidValueException) e.getCause();
-				response = new ApiResponse(invalidValueException.getCode(), invalidValueException.getMessage());
+				response = new ApiResponseDto(invalidValueException.getCode(), invalidValueException.getMessage());
 			} else {
-				response = new ApiResponse(e.getCode(), e.getMessage());
+				response = new ApiResponseDto(e.getCode(), e.getMessage());
 			}
 			return ResponseEntity.badRequest().body(response);
 		}
