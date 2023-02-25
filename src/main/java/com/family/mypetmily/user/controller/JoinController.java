d808ffa0e3ca411484df08f7ed77ca5fc1dc4541
SuJeongGong <0_sujeong@naver.com>
2023-02-25 22:45:31 +0900

    style:requestBody 어노테이션 추가 #4
    추가가 되지 않아도 정상 동작하지만 명시적으로 requestBody 어노테이션을 추가함

--- src/main/java/com/family/mypetmily/user/controller/JoinController.java
+++ src/main/java/com/family/mypetmily/user/controller/JoinController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -45,7 +46,7 @@ public class JoinController {
 			@ApiResponse(code = 200, message = "회원가입 성공")
 	})
 	@PostMapping(value = "")
-	public ResponseEntity join(@Validated final JoinRequestFormDto joinRequestFormDto, BindingResult bindingResult) {
+	public ResponseEntity join(@Validated @RequestBody final JoinRequestFormDto joinRequestFormDto, BindingResult bindingResult) {
 
 		ApiResponseDto result = null;
 		if (bindingResult.hasErrors()) {
